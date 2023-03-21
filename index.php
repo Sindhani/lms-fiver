3c7d7d69642c0fec403c7e4fc2a3046e924cc1d0
Waheed Sindhani <waheed.eliccs@gmail.com>
2023-03-21 20:07:02 +0500

    changes done

--- index.php
+++ index.php
@@ -52,13 +52,13 @@ if (isset($_POST['login'])) {
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
     <style>
         .img-wrapper {
-            height: 100%;
+            height: 250px!important;
             overflow: hidden;
         }
 
         .img-wrapper img {
-            height: 100%;
-            object-fit: cover;
+            width: 100%;
+            object-fit: contain;
             object-position: center;
         }
     </style>
